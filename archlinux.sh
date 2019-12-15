2be4cfb19dffa8c607a6c73f99b2eae08465967a
wint3rmute <mateusz.baczek1998@gmail.com>
2019-12-15 17:43:14 +0100

    comment for the system-mounting script

--- archlinux.sh
+++ archlinux.sh
@@ -1,3 +1,9 @@
+# This script mounts just enough of dirs
+# for the system to work properly, though
+# this probably could use more investigation.
+# refer to `proot --help` to get more info
+# on mounting options
+
 proot -k 4 -r /mnt/base-us/documents/arch \
   -b /etc/host.conf \
   -b /etc/hosts \
